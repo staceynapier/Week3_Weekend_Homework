6d4171e9f669b313a1524d44aa714c532b8ebbed
staceynapier <staceynapier86@gmail.com>
2017-07-14 13:12:04 +0100

    Initial commit - set up sql file

---
+++ db/cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE if EXISTS tickets;
+DROP TABLE if EXISTS customers;
+DROP TABLE if EXISTS films;
+
+CREATE TABLE customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT2
+);
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT2
+);
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+  );
